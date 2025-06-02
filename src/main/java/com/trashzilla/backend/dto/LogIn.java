6d2a16dacf0af98d83ed887bf8aec40dd076de59
Nadrojria <loicdesbordes@yahoo.fr>
2025-06-02 13:40:51 +0200

    add: login postmapping

---
+++ src/main/java/com/trashzilla/backend/dto/LogIn.java
@@ -0,0 +1,13 @@
+package com.trashzilla.backend.dto;
+
+public class LogIn {
+    private String email;
+    private String password;
+
+    // Getters et setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
